c2a3cb4dc0089931fac06120886dadb277eb4e2d
Momita Pal <79890792+Momita19@users.noreply.github.com>
2023-04-20 14:32:29 +0530

    Time: 23 ms (97.81%), Space: 13.8 MB (97.31%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -1,17 +1,26 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        i = 0
-        j =0
-        string = []
-        while i< len(word1) or j< len(word2):
-            if i<len(word1):
-                string += word1[i]
-                i += 1
-            if j <len(word2):
-                string += word2[j]
-                j += 1
-        return ''.join(string)
-            
+
+        return ''.join(i+j for i,j in itertools.zip_longest(word1, word2, fillvalue=''))
+    
+    
+    
+    
+    
+    
+    
+           #         i = 0
+#         j =0
+#         string = []
+#         while i< len(word1) or j< len(word2):
+#             if i<len(word1):
+#                 string += word1[i]
+#                 i += 1
+#             if j <len(word2):
+#                 string += word2[j]
+#                 j += 1
+                
+#             print(string)
         
         
         
