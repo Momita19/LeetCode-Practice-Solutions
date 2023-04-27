bbdca96f66a7a944ccaf4f0f203a8c524e2d7d89
Momita Pal <79890792+Momita19@users.noreply.github.com>
2023-04-27 16:18:21 +0530

    Time: 35 ms (29.16%), Space: 16.2 MB (34.09%) - LeetHub

---
+++ 0319-bulb-switcher/0319-bulb-switcher.py
@@ -0,0 +1,43 @@
+import math
+class Solution:
+    def bulbSwitch(self, n: int) -> int:
+        return int(math.sqrt(n))
+
+        
+#         Test cases
+#         1 2 3 4 5 6 7 8 9 10  = 3
+#         0 0 0 0 0 0 0 0 0 0
+#         1 1 1 1 1 1 1 1 1 1
+#         1 0 1 0 1 0 1 0 1 0
+#         1 0 0 0 0 1 1 0 0 0 
+   
+        
+#         1 2 3 4 5 6 7 8 9 10 11 = 3
+#         0 0 0 0 0 0 0 0 0 0 0
+#         1 1 1 1 1 1 1 1 1 1 1
+#         1 0 1 0 1 0 1 0 1 0 1
+#         1 0 1 0 1 1 0 0 0 0 0
+#         1 0 1 1 0 1 1 1 0 0 1
+#         1 0 1 0 1 1 1 1 0 1 1
+#         1 0 1 0 0 0 1 1 0 1 1
+#         1 0 1 0 0 0 0 1 0 1 1
+#         1 0 1 0 0 0 0 0 0 1 1
+#         1 0 1 0 0 0 0 0 1 1 1
+#         1 0 1 0 0 0 0 0 1 0 1
+#         1 0 1 0 0 0 0 0 1 0 0
+        
+#  if sqaures root 4,9,16,36 in between these it will return the square root of no till another higher square root
+#  till 4 the ans will be 1, after this ans will be 2 till 9 then 3 till 16 and so on
+
+# root_list = []
+#         i = 1
+#         while i**2 <= n:
+#             root_list.append(math.sqrt(i))
+#             i += 1
+            
+#         return root_list
+            
+
+          
+        
+        
