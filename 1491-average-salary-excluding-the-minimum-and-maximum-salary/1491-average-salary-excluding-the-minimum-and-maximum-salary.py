b8a1cb6cefe8602427dbad24f13668f9447fe074
Momita Pal <79890792+Momita19@users.noreply.github.com>
2023-05-02 00:13:47 +0530

    Time: 50 ms (5.49%), Space: 16.2 MB (8.21%) - LeetHub

--- 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.py
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.py
@@ -6,11 +6,9 @@ class Solution:
         minSalary = max(salary)
         for i in range(0, len(salary)):
             sum_n = sum_n + salary[i]
-            print(sum_n)
+
             maxSalary = max(salary[i], maxSalary)
-            print(maxSalary)
             minSalary = min(salary[i], minSalary)
-            print(minSalary)
        
         return (sum_n - minSalary - maxSalary)/(len(salary)-2)
         
