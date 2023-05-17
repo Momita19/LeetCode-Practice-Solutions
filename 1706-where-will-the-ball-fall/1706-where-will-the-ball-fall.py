8db9ef826d0097641b40ee90e675fc0e3472c20b
Momita Pal <79890792+Momita19@users.noreply.github.com>
2023-05-17 19:29:19 +0530

    Time: 211 ms (28.26%), Space: 17 MB (8.37%) - LeetHub

---
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findBall(self, grid: List[List[int]]) -> List[int]:
+        m, n = len(grid), len(grid[0])
+
+        def dfs(i: int, j: int) -> int:
+            if i == m:
+                return j
+            elif 0 <= (j_new := j + grid[i][j]) < n and grid[i][j] == grid[i][j_new]:
+                return dfs(i + 1, j_new)
+            else:
+                return -1
+
+        return [dfs(0, j) for j in range(n)]
